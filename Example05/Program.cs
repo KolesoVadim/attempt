bed409923765ca6e082546e00a215a6d6f4d5008
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2022-09-14 17:45:50 +0300

    added 05

---
+++ Example05/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "mia")
+{
+    Console.WriteLine("yoop, Mia");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
