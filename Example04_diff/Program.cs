2b349a205dab1119c0ad6032cce5c0b80f3daf58
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2022-09-14 17:30:30 +0300

    added 04

--- Example04_diff/Program.cs
+++ Example04_diff/Program.cs
@@ -1,9 +1,11 @@
-﻿// int С ЦЕЛЫМИ ЧИСЛАМИ
-//int numberA = 15;
-//int numberB = 6;
-//Console.Write(numberA / numberB);
+﻿//int С ЦЕЛЫМИ ЧИСЛАМИ
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+Console.Write(numberA + numberB);
 
 // double  с вещественными числами == 2,4.
-double numberA = 69;
-double numberB = 6;
-Console.Write(numberA / numberB);
+//double numberA = 69;
+//double numberB = 6;
+//Console.Write(numberA / numberB);
