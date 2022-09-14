c94b902baa1ae205263f8d4fbd57607481d3a098
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2022-09-14 17:20:01 +0300

    added example04

---
+++ Example04_diff/Program.cs
@@ -0,0 +1,9 @@
+﻿// int С ЦЕЛЫМИ ЧИСЛАМИ
+//int numberA = 15;
+//int numberB = 6;
+//Console.Write(numberA / numberB);
+
+// double  с вещественными числами == 2,4.
+double numberA = 69;
+double numberB = 6;
+Console.Write(numberA / numberB);
